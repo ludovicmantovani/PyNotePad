e1c5308e671439d35252a13de9b00908db5dc2b9
ludovicmantovani <ludovic.mantov@gmail.com>
2024-06-19 10:42:41 +0200

    [UPDATE]: GUI file separation

--- pyNotePad.py
+++ pyNotePad.py
@@ -1,2 +1,15 @@
-if __name__ == '__main__':
-    print("Hello World")
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from GUI.pyNotePadGUI import Ui_MainWindow
+
+
+class NotePadWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.show()
+
+
+app = QApplication(sys.argv)
+window = NotePadWindow()
+sys.exit(app.exec_())
